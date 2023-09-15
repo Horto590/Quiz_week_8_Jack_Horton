c8089788c81e627f9d7a1a10c78882a39a90504d
Jack Horton <jack.horton@griffithuni.edu.au>
2023-09-15 18:46:41 +1000

    fixed a found mistake to climate_q2.py

--- climate_q2.py
+++ climate_q2.py
@@ -1,33 +1,28 @@
 import matplotlib.pyplot as plt
 import pandas as panda
-import sqlite3
 
 years = []
 co2 = []
 temp = []
 
-data_connecting = sqlite3.connect(r".\climate.db")
-cursor = data_connecting.cursor()
-cursor.execute("SELECT * FROM climatedata")
-
-data_frame = panda.DataFrame(cursor.fetchall(), columns=['Year', 'Co2', 'Temperature'])
+data_frame = panda.read_csv('.\climate.csv')
 # print(data_frame)
 
 years = data_frame['Year'].tolist()
-co2 = data_frame['Co2'].tolist()
+co2 = data_frame['CO2'].tolist()
 temp = data_frame['Temperature'].tolist()
 # print(years, co2, temp)
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_2.png")
 
